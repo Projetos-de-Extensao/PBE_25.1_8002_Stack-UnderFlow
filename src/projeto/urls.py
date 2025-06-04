0c40e17cfb58266e8624e94518722d0951217aea
Eduardo Peruzzo <eduardo.peruzzo05@gmail.com>
2025-06-04 18:04:28 -0300

    pequenas atualizações

--- src/projeto/urls.py
+++ src/projeto/urls.py
@@ -26,12 +26,5 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/token/', obtain_auth_token, name='api_token_auth'),
-
 ]
 
-INSTALLED_APPS = [
-    # ...outros apps...
-    'rest_framework',
-    'rest_framework.authtoken',
-    # ...outros apps...
-]
